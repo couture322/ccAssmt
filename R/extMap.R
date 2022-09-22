b327fb62dc3709cb6387f46b84d1d87108ab5bcb
Jessica Couture <jcouture@ucsb.edu>
2022-09-21 22:46:10 -0700

    initial commit

---
+++ R/extMap.R
@@ -0,0 +1,101 @@
+#' Title
+#'
+#' @param mpaShp shp file of the existing or proposed MPA
+#' @param buff distance around mpa shape file for which analysis is conducted (to calculate extent box, "ploArea"). In decimal degrees (numeric)
+#' @param zoomLat latitudinal buffer for the entire extent map in decimal degrees (size depends on context extent map scale)
+#' @param zoomLon longitudinal buffer for the entire extent map in decimal degrees (size depends on context extent map scale)
+#' @param corr indicates whether longitude "lonFx" needs to be corrected for crossing the 180 meridian, if no use NA
+#'
+#' @return extent map of the plot area around the mpaShp based on buff
+#' @export
+#'
+#' @examples
+
+extMapFunc<-function(mpaShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
+
+  mapItems<-mpaShpBbBase(mpaShp = mpaShp,
+                         buff=buff,
+                         corr=corr)
+
+  mpa=mapItems[1][[1]]
+  ploArea=mapItems[2][[1]]
+  geoShp=mapItems[3][[1]]
+
+  siteBB<-st_bbox(mpa)
+
+  bbDf<-data.frame(lon=c(as.numeric(ploArea$xmin),
+                         as.numeric(ploArea$xmax),
+                         as.numeric(ploArea$xmax),
+                         as.numeric(ploArea$xmin)),
+                   lat=c(as.numeric(ploArea$ymin),
+                         as.numeric(ploArea$ymin),
+                         as.numeric(ploArea$ymax),
+                         as.numeric(ploArea$ymax)))%>%
+    sfheaders::sf_polygon()%>%
+    st_set_crs(4326)
+
+
+
+  if(is.na(corr)) {
+    totArea<-st_bbox(c(xmin=as.numeric(siteBB$xmin-zoomLon),
+                       xmax=as.numeric(siteBB$xmax+zoomLon),
+                       ymin=as.numeric(siteBB$ymin-zoomLat),
+                       ymax=as.numeric(siteBB$ymax+zoomLat)),crs = st_crs(mpa))
+  } else {
+    totArea<-st_bbox(c(xmin=ifelse(as.numeric(siteBB$xmin)<0,
+                                   180+(180+as.numeric(siteBB$xmin)),
+                                   as.numeric(siteBB$xmin))-zoomLon,
+                       xmax=ifelse(as.numeric(siteBB$xmax)<0,
+                                   180+(180+as.numeric(siteBB$xmax)),
+                                   as.numeric(siteBB$xmax))+zoomLon,
+                       ymin=as.numeric(siteBB$ymin)-zoomLat,
+                       ymax=as.numeric(siteBB$ymax)+zoomLat),
+                     crs = st_crs(mpa))
+  }
+
+  ### crop base layers from world map
+  if(is.na(corr)){
+
+    geoShp<-map_data("world")%>%
+      filter(long>totArea$xmin,
+             long<totArea$xmax,
+             lat>totArea$ymin,
+             lat<totArea$ymax)%>%
+      group_by(subregion)%>%
+      mutate(dup=n()>3)%>%
+      filter(dup==TRUE)%>%
+      ungroup()
+
+  } else {
+
+    geoShp<-map_data("world",wrap=c(0,360))%>%
+      filter(long>totArea$xmin,
+             long<totArea$xmax,
+             lat>totArea$ymin,
+             lat<totArea$ymax)%>%
+      group_by(subregion)%>%
+      mutate(dup=n()>3)%>%
+      filter(dup==TRUE)%>%
+      ungroup()
+
+  }
+
+
+  extMap<-ggplot()+
+    geom_map(data=geoShp,map=geoShp,aes(map_id=region))+
+    # geom_sf(data=siteEez,fill="grey33",color=NA,alpha=0.3)+
+    geom_sf(data=mpa,fill="deepskyblue3",alpha=0.3,color="deepskyblue4",linetype=2)+
+    geom_sf(data=bbDf,fill=NA,color="darkorange3")+
+    annotate(geom="text", x=ploArea$xmin+5, y=ploArea$ymin-1, label="Area assessed",
+             color="darkorange3")+
+    scale_x_continuous(expand = c(0,0))+
+    scale_y_continuous(expand = c(0,0))+
+    coord_sf(crs = 4326,
+             xlim = c(totArea$xmin, totArea$xmax),
+             ylim = c(totArea$ymin, totArea$ymax))+
+    theme_bw()+
+    labs(title = "Extent map",x="longitude",y="latitude")
+
+  return(extMap)
+
+}
---
+++ R/mpaShpBbBase.R
@@ -0,0 +1,92 @@
+#' Title
+#'
+#' @param mpaShp shp file of the existing or proposed MPA
+#' @param buff distance around mpa shape file for which analysis is conducted (to calculate extent box, "ploArea"). In decimal degrees (numeric)
+#' @param corr indicates whether longitude "lonFx" needs to be corrected for crossing the 180 meridian, if no use NA
+#'
+#' @return list of 3 items: mpaShp projected to WGS84 and corrected to 360 if needed: corr="lonFx", bounding box for the plot area, base map of land within the plot area
+#' @export
+#'
+#' @examples
+mpaShpBbBase<-function(mpaShp,buff,corr=c(NA,"lonFx")){
+
+  ### format MPA shape file
+
+  if(is.na(st_crs(mpaShp))) {
+
+    mpa<-mpaShp%>%
+      st_set_crs(4326)#"+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+  } else {
+
+    mpa<-mpaShp%>%
+      st_transform(4326)
+  }
+
+  if(is.na(corr)) {
+
+    mpa<-mpa
+
+  } else {
+
+    mpa<-mpa%>%
+      st_cast(to="POINT")%>% # convert polygons to points
+      mutate(lon=st_coordinates(geometry)[,"X"] ,
+             lat=st_coordinates(geometry)[,"Y"])%>%
+      mutate(lon2=case_when(lon < 0 ~ 180+(180+lon),
+                            TRUE ~ lon))%>%
+      st_drop_geometry()%>%
+      sfheaders::sf_multipolygon(x="lon2",y="lat",multipolygon_id=colnames(mpaShp)[1])%>%
+      st_set_crs(4326)
+  }
+
+
+  siteBB<-st_bbox(mpa)
+
+  if(is.na(corr)) {
+    ploArea<-st_bbox(c(xmin=as.numeric(siteBB$xmin-buff),
+                       xmax=as.numeric(siteBB$xmax+buff),
+                       ymin=as.numeric(siteBB$ymin-buff),
+                       ymax=as.numeric(siteBB$ymax+buff)),
+                     crs = st_crs(mpa))
+  } else {
+    ploArea<-st_bbox(c(xmin=as.numeric(ifelse(siteBB$xmin<0,
+                                              180+(180+siteBB$xmin),
+                                              siteBB$xmin)-buff),
+                       xmax=as.numeric(ifelse(siteBB$xmax<0,
+                                              180+(180+siteBB$xmax),
+                                              siteBB$xmax)+buff),
+                       ymin=as.numeric(siteBB$ymin-buff),
+                       ymax=as.numeric(siteBB$ymax+buff)),
+                     crs = st_crs(mpa))
+  }
+
+
+  ### crop base layers from world map
+  if(is.na(corr)){
+
+    geoShp<-map_data("world")%>%
+      filter(long>ploArea$xmin,
+             long<ploArea$xmax,
+             lat>ploArea$ymin,
+             lat<ploArea$ymax)%>%
+      group_by(subregion)%>%
+      mutate(dup=n()>3)%>%
+      filter(dup==TRUE)%>%
+      ungroup()
+
+  } else {
+
+    geoShp<-map_data("world",wrap=c(0,360))%>%
+      filter(long>ploArea$xmin,
+             long<ploArea$xmax,
+             lat>ploArea$ymin,
+             lat<ploArea$ymax)%>%
+      group_by(subregion)%>%
+      mutate(dup=n()>3)%>%
+      filter(dup==TRUE)%>%
+      ungroup()
+
+  }
+
+  return(list(mpa,ploArea,geoShp))
+}
