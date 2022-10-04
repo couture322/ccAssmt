b95e51925f41dfe7dcf06271ede6cc4e3edab555
Jessica Couture <jcouture@ucsb.edu>
2022-10-04 11:03:12 -0700

    adds eez boundaries to the extent map function and output

--- R/extMap.R
+++ R/extMap.R
@@ -1,8 +1,7 @@
 #' @title Extent Map
 #' @description takes a MPA shapefile and creates an extent map showing the area assessed based on the established plot area
 #'
-#' @param mpaShp shp file of the existing or proposed MPA
-#' @param eezShp shp file of the relevant EEZ as produced in mpaShpBbBase function (list item 4)
+#' @param mpaShp shp file of the existing or proposed MPA, creates different layers using mpaShpBbBase function
 #' @param buff distance around mpa shape file for which analysis is conducted (to calculate extent box, "ploArea"). In decimal degrees (numeric)
 #' @param zoomLat latitudinal buffer for the entire extent map in decimal degrees (size depends on context extent map scale)
 #' @param zoomLon longitudinal buffer for the entire extent map in decimal degrees (size depends on context extent map scale)
@@ -12,9 +11,9 @@
 #' @export
 #'
 #' @examples
-#' extMap(mpaShp=revMPA,eezShp=mxMap[4][[1]], buff=7,zoomLat=15,zoomLon=10,corr=NA)
+#' extMap(mpaShp=revMPA, buff=7,zoomLat=15,zoomLon=10,corr=NA)
 
-extMap<-function(mpaShp,eezShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
+extMap<-function(mpaShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
 
   mapItems<-mpaShpBbBase(mpaShp = mpaShp,
                          buff=buff,
@@ -23,6 +22,7 @@ extMap<-function(mpaShp,eezShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
   mpa=mapItems[1][[1]]
   ploArea=mapItems[2][[1]]
   geoShp=mapItems[3][[1]]
+  siteEez<-mapItems[4][[1]]
 
   siteBB<-st_bbox(mpa)
 
@@ -86,7 +86,7 @@ extMap<-function(mpaShp,eezShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
 
   extMap<-ggplot()+
     geom_map(data=geoShp,map=geoShp,aes(map_id=region))+
-    # geom_sf(data=siteEez,fill="grey33",color=NA,alpha=0.3)+
+    geom_sf(data=siteEez,fill=NA,color="navy")+
     geom_sf(data=mpa,fill="deepskyblue3",alpha=0.3,color="deepskyblue4",linetype=2)+
     geom_sf(data=bbDf,fill=NA,color="darkorange3")+
     annotate(geom="text", x=ploArea$xmin+5, y=ploArea$ymin-1, label="Area assessed",
@@ -97,7 +97,7 @@ extMap<-function(mpaShp,eezShp,buff,zoomLat,zoomLon,corr=c(NA,"lonFx")){
              xlim = c(totArea$xmin, totArea$xmax),
              ylim = c(totArea$ymin, totArea$ymax))+
     theme_bw()+
-    labs(title = "Extent map",x="longitude",y="latitude")
+    labs(title = "Assessment extent",x="longitude",y="latitude")
 
   return(extMap)
 
