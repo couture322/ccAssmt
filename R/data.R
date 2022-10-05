7b15fdff8561254e3e93fbaa7f5a9cf23cd26691
Jessica Couture <jcouture@ucsb.edu>
2022-10-05 14:09:30 -0700

    Adds example data (revMPA) and documentation file for all included datasets

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Marine Regions EEZ Boundaries Data
+#'
+#' Spatial features (sf) data frame with the Exclusive Economic Zones for all countries around the world for plotting purposes. Data are stored as lines.
+#' @format A spatial feature dataset with several identifying columns, and line (MULTISTRING) geometries. Contains 27 variables of 2215 objects.
+"mrEEZ"
+
+#' Revillagigedo MPA Spatial data
+#'
+#' Spatial features (sf) data frame of the Revillagigedo MPA off the Pacific coast of Mexico.
+#' @format A spatial feature dataset with several identifying columns, and line (MULTISTRING) geometries. Contains 31 variables of 2 objects.
+"revMPA"
