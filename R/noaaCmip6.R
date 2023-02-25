3b3045c66376926e24cd20b5da3ae6af9d8f57ac
Jessica Couture <jcouture@ucsb.edu>
2023-02-24 17:42:53 -0800

    starts debugging noaaCMIP6 function, needs more work

--- R/noaaCmip6.R
+++ R/noaaCmip6.R
@@ -2,6 +2,7 @@
 #' @description Pulls NOAA data from author's private drive folder and plots up variable (temperature (tos), salinity (sos), oxygen at 2 depths (o2)) projections for the assessment area indicated
 #'
 #' @param mpaShp shp file of the existing or proposed MPA
+#' @param eezShp shp file of relevant EEZ, if none enter NA
 #' @param buff distance around mpa shape file for which analysis is conducted (to calculate extent box, "ploArea"). In decimal degrees (numeric)
 #' @param futYr year to capture variable projection. Data range from 2015-2100
 #' @param var one of "tos" (SST), "sos" (SSSal), "o2s" ([O2] shallow), "o2d" ([O2] deep) variables to plot.SST and SSS data are by definition only calculated at one depth (0m) and oxygen concentration is assessed at shallow and deep
@@ -14,14 +15,16 @@
 #' noaaCMIP6(mpaShp=revMPA,buff=7,var="tos",corr=NA)
 #'
 noaaCMIP6<-function(mpaShp,
-                    buff,
-                    futYr,
-                    var=c("tos","sos","o2"),
-                    corr=c(NA,"lonFx")) {
+                     eezShp,
+                     buff,
+                     futYr,
+                     var=c("tos","sos","o2"),
+                     corr=c(NA,"lonFx")) {
 
 
   ### create map layers
   mapItems<-mpaShpBbBase(mpaShp = mpaShp,
+                         eezShp = eezShp,
                          buff=buff,
                          corr=corr)
 
@@ -33,9 +36,6 @@ noaaCMIP6<-function(mpaShp,
   lyrIndx<-c(60:72,(((futYr-2015)*12)+1):(((futYr-2015)*12)+12))
 
   ### pull in global data from the drive based on indicated variable (var)
-  dir.create("data/tempDat")
-  datFile<-"data/tempDat/noaaTmp.nc"
-
 
   driveFile<-if(var=="tos") {
     "tos_Omon_ACCESS-CM2_ssp126_r1i1p1f1_gn_201501-210012.nc"
@@ -47,9 +47,11 @@ noaaCMIP6<-function(mpaShp,
     }
   }
 
-  drive_download(driveFile,
-                 path=datFile,
-                 overwrite = TRUE)
+  # drive_download(driveFile,
+  #                path=datFile,
+  #                overwrite = TRUE)
+
+  datFile<-paste("../../../../sandbox-sparc/cmip6_data/noaaCortadv6",driveFile,sep="/")
 
   # ncDat<-nc_open(datFile)
   noaaDat<-terra::rast(datFile)
@@ -60,7 +62,7 @@ noaaCMIP6<-function(mpaShp,
     ext(noaaDat)<-c(-180,180,-90,90)
 
 
-    } else {
+  } else {
     ext(noaaDat)<-c(0,360,-90,90)
   }
 
@@ -83,14 +85,14 @@ noaaCMIP6<-function(mpaShp,
 
     noaa2<-subset(noaaSub,2)-subset(noaaSub,1)
 
-}
+  }
 
   noaaDiff<-noaa2%>%
     as.data.frame(.,xy=TRUE)%>% ## convert to df
-    filter(lon>ploArea$xmin,
-           lon<ploArea$xmax,
-           lat>ploArea$ymin,
-           lat<ploArea$ymax)
+    filter(x>ploArea$xmin,
+           x<ploArea$xmax,
+           y>ploArea$ymin,
+           y<ploArea$ymax)
   colnames(noaaDiff)<-c("lon","lat","diff")
 
   ### plot data
@@ -106,39 +108,32 @@ noaaCMIP6<-function(mpaShp,
       }
     }
 
-    if(any(noaaDiff$diff > 0) & any(noaaDiff$diff < 0)) {
-      sclFllGrd<-scale_fill_gradient2(low = "#0a6165",high = "#da4325",mid = "#e7e1bc",midpoint = 0,
-                                      name=paste("change\n2020 -", futYr))
+  if(any(noaaDiff$diff > 0) & any(noaaDiff$diff < 0)) {
+    sclFllGrd<-scale_fill_gradient2(low = "#0a6165",high = "#da4325",mid = "#e7e1bc",midpoint = 0,
+                                    name=paste("change\n2020 -", futYr))
+  } else {
+
+    if(all(noaaDiff$diff>=0)) {
+      sclFllGrd<-scale_fill_gradient(high = "#da4325",low = "#e7e1bc",
+                                     name=paste("change\n2020 -", futYr))
     } else {
-
-      if(all(noaaDiff$diff>=0)) {
-        sclFllGrd<-scale_fill_gradient(high = "#da4325",low = "#e7e1bc",
-                                       name=paste("change\n2020 -", futYr))
-      } else {
-        sclFllGrd<-scale_fill_gradient(low = "#0a6165",high = "#e7e1bc",
-                                       name=paste("change\n2020 -", futYr))
-      }
+      sclFllGrd<-scale_fill_gradient(low = "#0a6165",high = "#e7e1bc",
+                                     name=paste("change\n2020 -", futYr))
     }
+  }
 
-    sitePlt<-ggplot()+
-      geom_rast(data=noaaDiff,aes(x=lon,y=lat,fill=diff),width=1,height=1)+
-      geom_sf(data=mpa,color="grey21",fill=NA)+
-      geom_sf(data=geoShp)+
-      geom_sf(data=siteEez,color="navy",fill=NA)+
-      sclFllGrd+
-      scale_x_continuous(expand = c(0,0))+
-      scale_y_continuous(expand = c(0,0),limits = c(ploArea$ymin,ploArea$ymax+0.1))+
-      theme(legend.direction = "vertical",legend.position = "right")+
-      ggtitle(title=pltTitl,subtitle = pltSubTitl)+
-      theme_bw()
+  sitePlt<-ggplot()+
+    geom_raster(data=noaaDiff,aes(x=lon,y=lat,fill=diff),width=1,height=1)+
+    geom_sf(data=mpa,color="grey21",fill=NA)+
+    geom_map(data=geoShp,map=geoShp,aes(map_id=region))+
+    geom_sf(data=siteEez,color="navy",fill=NA)+
+    sclFllGrd+
+    scale_x_continuous(expand = c(0,0))+
+    scale_y_continuous(expand = c(0,0),limits = c(ploArea$ymin,ploArea$ymax+0.1))+
+    theme(legend.direction = "vertical",legend.position = "right")+
+    labs(title=pltTitl,subtitle = pltSubTitl)+
+    theme_bw()
 
-    ### delete large file from local
-
-    if (file.exists(datFile)) {
-      unlink(datFile)
-      cat(paste("The file has been deleted: ",datFile))
-    }
-
-    return(sitePlt)
+  return(sitePlt)
 
 }
